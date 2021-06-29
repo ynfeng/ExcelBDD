9a888ae40819aa1bbb8189832a56bca1511dad93
ynfeng <fynwin@gmail.com>
2021-06-29 20:06:16 +0800

    refactor.

--- JavaExcelBDD/src/main/java/com/excelbdd/Rows.java
+++ JavaExcelBDD/src/main/java/com/excelbdd/Rows.java
@@ -1,14 +1,12 @@
 package com.excelbdd;
 
 import java.util.List;
+import java.util.Objects;
 import org.apache.commons.compress.utils.Lists;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
 public class Rows {
-    private int headerRow;
-    private char parameterNameColumn;
-    private String columnType;
     private final List<Row> rows = Lists.newArrayList();
 
     public void loadRows(XSSFSheet xssfSheet) {
@@ -19,24 +17,30 @@ public class Rows {
             }
             Row row = new Row(iRow, rowCurrent);
             rows.add(row);
-            headerRow = row.headerRowNumber();
-            parameterNameColumn = row.parameterNameColumnName();
-            columnType = row.rowType();
-            if (columnType != null) {
-                break;
-            }
         }
     }
 
     public int headerRowNumber() {
-        return headerRow;
+        return rows.stream()
+            .map(Row::headerRowNumber)
+            .filter(rowNumber -> rowNumber != -1)
+            .findAny()
+            .orElseThrow(() -> new IllegalStateException("header not found."));
     }
 
     public char parameterNameColumnName() {
-        return parameterNameColumn;
+        return rows.stream()
+            .map(Row::parameterNameColumnName)
+            .filter(columnName -> columnName != '@')
+            .findAny()
+            .orElseThrow(() -> new IllegalStateException("parameter name column not found."));
     }
 
     public String rowType() {
-        return columnType;
+        return rows.stream()
+            .map(Row::rowType)
+            .filter(Objects::nonNull)
+            .findAny()
+            .orElseThrow(() -> new IllegalStateException("has not row type."));
     }
 }
